4eb6a00c83a5e1760588ed2781076a23a54f56a2
Shiv-ka-ansh <shivanshg2002@gmail.com>
2026-01-07 17:04:52 +0530

    feat: Add Docker support by introducing Dockerfile, .dockerignore, and configuring Next.js for standalone output.

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   images: {
-    domains: ['example.com', '13.127.80.211','dibstestbucket0403.s3.ap-south-1.amazonaws.com',],
+    domains: ['example.com', '13.127.80.211', 'dibstestbucket0403.s3.ap-south-1.amazonaws.com',],
     remotePatterns: [
       {
         protocol: 'https',
--- tests/onboard.spec.ts
+++
@@ -1,7 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('onboard hotel', async ({ page }) => {
-  // Needs credentials to be useful
-  await page.goto('http://localhost:3000');
-  await page.screenshot({ path: 'screenshot_landing.png' });
-});
